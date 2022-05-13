4c03d47f64b21b22c65f467ccc8c34b5cf4f37c0
santocaridi99 <santocaridi99@gmail.com>
2022-05-13 17:29:35 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        private int code;
+        private string nome;
+        public string description;
+        public double price;
+        public double iva;
+       
+        //faccio i get e i set 
+        // Get=lettura prendo il valore della variabile
+        // Set = Viene eseguito al momento della scrittura , set viene eseguito quando viene assegnato un Nuovo valore alla variabile
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }   
+        }
+
+        public string Nome
+        {
+            get { return nome; }    
+            set { nome = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }     
+            set { description = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public double Iva
+        {
+            get { return iva; }     
+            set { iva = value; }
+        }
+
+        private Random randomNum;
+
+        //costruttore 
+        public Prodotto(string nome , string description , double price , double iva)
+        {
+            randomNum = new Random();
+            Code = randomNum.Next();
+            this.Nome = nome;
+            this.Description = description;
+            this.Price = price;
+            this.Iva = iva;
+        }
+
+        public double IvaPrice()
+        {
+            return Price + Iva ;
+        }
+
+        public string ExtendedName()
+        {
+            string extendedName;
+            return extendedName = Code + Nome;
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csharp_oop_shop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodotto prodotto1 = new Prodotto("pallina", "una pallina", 2, 1);
+            Console.WriteLine("il prodotto  è {0}", prodotto1.ExtendedName());
+            Console.WriteLine("nome prodotto è {0}",prodotto1.Nome);
+            Console.WriteLine("Descrizione: {0}", prodotto1.Description);
+            Console.WriteLine("Il prezzo del prodotto è {0} euro", prodotto1.Price);
+            Console.WriteLine("l'iva del prodotto {0} euro ", prodotto1.Iva);
+            Console.WriteLine("Prezzo Iva inclusa è  {0} euro", prodotto1.IvaPrice());
+
+
+            Console.WriteLine("--------------------------------------------------------");
+
+
+            Prodotto prodotto2 = new Prodotto("racchetta", "una racchetta", 20, 1);
+            Console.WriteLine("il prodotto è {0}", prodotto2.ExtendedName());
+            Console.WriteLine("nome prodotto è {0}", prodotto2.Nome);
+            Console.WriteLine("Descrizione: {0}", prodotto2.Description);
+            Console.WriteLine("Il prezzo del prodotto è {0} euro", prodotto2.Price);
+            Console.WriteLine("l'iva del prodotto {0} euro ", prodotto2.Iva);
+            Console.WriteLine("Prezzo Iva inclusa è  {0} euro", prodotto2.IvaPrice());
+        }
+    }
+}
